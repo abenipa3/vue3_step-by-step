e557e121e07d079632d99c69f165c4eb2989c8ae
alyssabenipayo <alyssa.benipayo@nelnet.net>
2023-02-20 16:56:09 -0800

    Episode 6: Saving Progress.

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -3,13 +3,20 @@
 
 export default {
     template: `
-            <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+            <button 
+            :class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" 
+            :disabled="processing">
                 <slot />
             </button>
         `,
 
         props: {
-            type: String
+            // Name of the prop
+            type: {
+                // Type of the prop
+                type: String,
+                default: 'primary'
+            }
         },
 
     data() {
