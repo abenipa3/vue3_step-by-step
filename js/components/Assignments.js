fabcff785bc0b6f5f0f274bb5e8e809b7752edb9
alyssabenipayo <alyssa.benipayo@nelnet.net>
2023-02-22 22:40:26 -0800

    Episode 8: Saving Progress.

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,8 +5,15 @@ export default {
 
     template: `
     <section class="space-y-6">
-    <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
-    <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+        <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+        <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+        <form @submit="add">
+        <div class="border border-gray-600 text-black">
+            <input placeholder="New Assignment..." class="p-2" />
+            <button type="submit" class="bg-white p-2 border-l">Add</button>
+        </div>
+        </form>
     </section>
     `,
     data() {
@@ -18,12 +25,22 @@ export default {
             ]
         }
     },
+
     computed: {
         filters() {
             return{
                 inProgress: this.assignments.filter(assignment => !assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete),
             };
+        }
+    },
+
+    methods: {
+        // Prevents page from refreshing after submit!
+        add(e) {
+            e.preventDefault();
+             
+            alert('hi there');
         }
     }
 }
