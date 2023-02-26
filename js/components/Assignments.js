6c166c22e5cb840c0c004c3cb2c5f295b4954a0a
alyssabenipayo <alymaebenipayo@gmail.com>
2023-02-25 17:48:03 -0800

    Episode 9: Saving Progress.

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,19 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-    components: { AssignmentList },
+    components: { AssignmentList, AssignmentCreate },
 
     template: `
     <section class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-        <form @submit.prevent="add">
-        <div class="border border-gray-600 text-black">
-            <input v-model="newAssignment" placeholder="New Assignment..." class="p-2" />
-            <button type="submit" class="bg-white p-2 border-l">Add</button>
-        </div>
-        </form>
+        <assignment-create :assignments="assignments"></assignment-create>
     </section>
     `,
     data() {
@@ -23,7 +19,6 @@ export default {
                 { name: "Read Chapter 4", complete: false, id: 2 },
                 { name: "Turn in Homework", complete: false, id: 3 },
             ],
-            newAssignment: ''
         }
     },
 
@@ -35,17 +30,4 @@ export default {
             };
         }
     },
-
-    methods: {
-        // Prevents page from refreshing after submit!
-        add() {
-            this.assignments.push({
-                name: this.newAssignment,
-                completed: false,
-                id: this.assignments.length + 1
-            });
-
-            this.newAssignment = '';
-        }
-    }
 }
