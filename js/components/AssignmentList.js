b8e6c42b3afa8b40e11e483c4007cfeee75db755
alyssabenipayo <alymaebenipayo@gmail.com>
2023-02-25 23:19:23 -0800

    Episode 10 Completed.

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -16,6 +16,9 @@ export default {
             @click="currentTag = tag"
             v-for="tag in tags" 
             class="border rounded px-1 py-px text-xs"
+            :class="{
+                'border-blue-500 text-blue-500': tag === currentTag
+            }"
 
             >{{ tag }}</button>
     </div>
